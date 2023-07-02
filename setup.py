2de95aefffa274eada60f3ddf8d53ba9f3f1cbb2
Binga-a <shubhamyadav.2508@gmail.com>
2023-07-02 17:24:21 +0530

    Code Updated

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
-HYPHEN_E_DOT = '-e .'
+HYPEN_E_DOT='-e .'
 
-def get_requirements(file_path:str)-> List[str]:
+def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements=[req.replace('\n','') for req in requirements]
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
-        if HYPHEN_E_DOT in requirements:
-                requirements.remove(HYPHEN_E_DOT)
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
 
         return requirements
-    
+
 
 setup(
-    name = 'DiamondPricePrediction',
-    version= '0.0.1',
-    author = 'Shubham',
-    author_email= 'shubham.leo7@gmail.com',
-    install_requires = get_requirements('requirements.txt'),
+    name='DiamondPricePrediction',
+    version='0.0.1',
+    author='Krish',
+    author_email='krish.naik@pw.live',
+    install_requires=get_requirements('requirements.txt'),
     packages=find_packages()
+
 )
